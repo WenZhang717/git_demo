cfe8d0f945dc7d8650e1b19e16b8a31aa5a426ed
Zhangwen <zhangwen@126.com>
2019-08-23 23:13:57 +0800

    test stash

--- 1.py
+++ 1.py
@@ -1,8 +1,11 @@
 a = 1;
 I went back to change 1 and modify
 I was changed in dev branch
-<<<<<<< HEAD
+
 Edit in master branch
-=======
+
 Edit in branch
->>>>>>> edit in dev
+
+feel happy
+
+add oil
